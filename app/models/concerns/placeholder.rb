a5b2266aaefff2f478da95d5adbde70f2e7942e3
Alex Calaca <alexcalaca@gmail.com>
2018-04-08 04:24:16 +0000

    Implemented concers for managing image  data

---
+++ app/models/concerns/placeholder.rb
@@ -0,0 +1,8 @@
+module PlaceHolder
+  extend ActiveSupport::Concern
+  
+  def self.image_generator(height:, width:)
+      "http://placehold.it/#{height}x#{width}"
+    
+  end
+end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,14 @@
 class Skill < ApplicationRecord
+  include PlaceHolder
   
   validates_presence_of :title, :percent_utilized
+  
+  def set_defaults
+    self.badge ||= PlaceHolder.image_generator(height: '250', width: '250')
+  end
+  
+  
+  
 end
 
 
