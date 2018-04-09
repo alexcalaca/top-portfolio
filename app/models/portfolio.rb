41a03b3da062bbd2d63bd62563aa5565a4f10e59
Alex Calaca <alexcalaca@gmail.com>
2018-04-09 15:08:27 +0000

    Integrated techologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
-  include PlaceHolder
+  has_many :technologies
+  include Placeholder
   
   validates_presence_of :title, :body, :thumb_image, :main_image
   
@@ -12,8 +13,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
-    self.main_image ||= PlaceHolder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= PlaceHolder.image_generator(height: '350', width: '200')
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
   
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,10 @@
 class Skill < ApplicationRecord
-  include PlaceHolder
+  include Placeholder
   
   validates_presence_of :title, :percent_utilized
   
   def set_defaults
-    self.badge ||= PlaceHolder.image_generator(height: '250', width: '250')
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
   
   
